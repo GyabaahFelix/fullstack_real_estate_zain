9bb943155ba535c9483be270b4036013ce5c8a5f
Felixic99 <fgyabaah003@st.ug.edu.gh>
2024-09-01 20:17:03 +0000

    Add files via upload

---
+++ client/src/pages/Property/Property.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Property.css";
+import { useQuery } from "react-query";
+import { useLocation } from "react-router-dom";
+import { getProperty } from "../../utils/api";
+import { PuffLoader } from "react-spinners";
+import { AiFillHeart } from "react-icons/ai";
+const Property = () => {
+  const { pathname } = useLocation();
+  const id = pathname.split("/").slice(-1)[0];
+  const { data, isLoading, isError } = useQuery(["resd", id], () =>
+    getProperty(id)
+  );
+  console.log(data);
+  if (isLoading) {
+    return (
+      <div className="wrapper">
+        <div className="flexCenter paddings">
+          <PuffLoader />
+        </div>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="wrapper">
+        <div className="flexCenter paddings">
+          <span>Error while fetching the property details</span>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="flexColStart paddings innerWidth property-container">
+        {/* like button */}
+        <div className="like">
+          <AiFillHeart size={24} color="white" />
+        </div>
+
+        {/* image */}
+        <img src={data?.image} alt="home image" />
+
+        <div className="flexCenter property-details">
+          {/* left */}
+          <div className="flexColStart left">
+            {/* head */}
+            <div className="flexStart head">
+              <span className="primaryText">{data.title}</span>
+              <span className="orangeText" style={{fontSize: '1.5rem'}}>${data.price}</span>
+            </div>
+          </div>
+
+
+
+          {/* right side */}
+          <div className="right">
+            this is right side
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Property;
